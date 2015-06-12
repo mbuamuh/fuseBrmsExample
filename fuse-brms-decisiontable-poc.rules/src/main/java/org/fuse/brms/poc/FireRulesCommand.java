4736849a298e32ebc48a9c7d5cfc38bf2133a9a2
mbuamuh <ugwem@yahoo.com>
2015-06-12 15:09:14 +0100

    modified pom.xml to import execution implementatio 'wq' is not recognized as an internal or external command, operable program or batch file.

--- fuse-brms-decisiontable-poc.rules/src/main/java/org/fuse/brms/poc/FireRulesCommand.java
+++ fuse-brms-decisiontable-poc.rules/src/main/java/org/fuse/brms/poc/FireRulesCommand.java
@@ -10,6 +10,7 @@ import org.drools.core.command.runtime.BatchExecutionCommandImpl;
 import org.drools.core.command.runtime.rule.FireAllRulesCommand;
 import org.drools.core.command.runtime.rule.InsertObjectCommand;
 
+import poc.decisiontable.fuse.brms.displayproduct.GetProductsRequest;
 import poc.decisiontable.fuse.brms.displayproduct.ProductDetails;
 import poc.decisiontable.fuse.brms.displayproduct.UserDetails;
 
@@ -18,14 +19,16 @@ public class FireRulesCommand {
 
     public void insertAndFireRules(Exchange exchange) {
         Message in = exchange.getIn();
-       ProductDetails product = in.getBody(ProductDetails.class);
-       UserDetails user = in.getBody(UserDetails.class);
+        GetProductsRequest getProductsRequest = in.getBody(GetProductsRequest.class);
+       ProductDetails product = getProductsRequest.getProduct();
+       UserDetails user = getProductsRequest.getUser();
 
         BatchExecutionCommandImpl command = new BatchExecutionCommandImpl();
         final List<GenericCommand<?>> commands = command.getCommands();
         commands.add(new InsertObjectCommand(product, "product"));
         commands.add(new InsertObjectCommand(user, "user"));
         commands.add(new FireAllRulesCommand());
+       
 
         in.setBody(command);
     }
--- fuse-brms-decisiontable-poc.webservice/src/main/java/org/fuse/brms/poc/converter/GetProductsResponseConverter.java
+++ fuse-brms-decisiontable-poc.webservice/src/main/java/org/fuse/brms/poc/converter/GetProductsResponseConverter.java
@@ -6,6 +6,7 @@ import poc.decisiontable.fuse.brms.displayproduct.GetProductsRequest;
 import poc.decisiontable.fuse.brms.displayproduct.GetProductsResponse;
 import poc.decisiontable.fuse.brms.displayproduct.ProductDetails;
 import poc.decisiontable.fuse.brms.displayproduct.UserDetails;
+import org.drools.core.runtime.impl.ExecutionResultImpl;
 
 @Converter
 public class GetProductsResponseConverter {
@@ -14,24 +15,39 @@ public class GetProductsResponseConverter {
 	    }
 	 
 	 @Converter
-	 public static GetProductsResponse convert(GetProductsRequest input){
+	 public static GetProductsResponse convert(ExecutionResultImpl executionResultImpl){
 		 GetProductsResponse response = null;
-		 
-		 if(input==null){
+		 if (executionResultImpl==null){
 			 return response;
 		 }
+		 ProductDetails product = (ProductDetails) executionResultImpl.getValue("product");
+		 UserDetails user = (UserDetails) executionResultImpl.getValue("user");
+		 GetProductsRequest input = null;
+		 response = new GetProductsResponse();
+		 if(product!=null){
+				response.setProduct(product );
+				  }
 		 
-		  response = new GetProductsResponse();
-		  
-		  ProductDetails product = input.getProduct();
-		  if(product!=null){
-		response.setProduct(product );
-		  }
-		
-		 UserDetails user = input.getUser();
-		  if(user!=null){
+		 if(user!=null){
 			  response.setUser(user);
 		  }
+		
+		 
+//		 if(input==null){
+//			 return response;
+//		 }
+//		 
+//		  response = new GetProductsResponse();
+//		  
+//		  ProductDetails product = input.getProduct();
+//		  if(product!=null){
+//		response.setProduct(product );
+//		  }
+//		
+//		 UserDetails user = input.getUser();
+//		  if(user!=null){
+//			  response.setUser(user);
+//		  }
 		  
 		 return response;
 	 }
